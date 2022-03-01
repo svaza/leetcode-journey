4b86613dd3eab58779f76a9ec8f36f72be76860f
vazasantosh@gmail.com <vazasantosh@gmail.com>
2022-02-28 22:49:30 -0600

    sum of linked list

---
+++ c#/SumOfLinkedList/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System;
+using static System.Console;
+
+public class Program
+{
+  // This is an input class. Do not edit.
+  public class LinkedList
+  {
+    public int value;
+    public LinkedList next;
+
+    public LinkedList(int value)
+    {
+      this.value = value;
+      this.next = null;
+    }
+  }
+
+  public LinkedList SumOfLinkedLists(LinkedList linkedListOne, LinkedList linkedListTwo)
+  {
+
+    int carry = 0;
+    LinkedList linkedListSumHead = null;
+    LinkedList runningSum = null;
+    while (linkedListOne != null || linkedListTwo != null)
+    {
+      int sum = (linkedListOne?.value ?? 0) + (linkedListTwo?.value ?? 0) + carry;
+			
+      if (sum > 9)
+      {
+				carry = sum / 10;
+        sum = sum % 10;
+      }
+      else
+      {
+        carry = 0;
+      }
+			
+      var localSum = new LinkedList(sum);
+      if(runningSum != null) runningSum.next = localSum;
+      runningSum = localSum;
+
+      if(linkedListSumHead == null) linkedListSumHead = runningSum;
+     
+      linkedListOne = linkedListOne?.next;
+      linkedListTwo = linkedListTwo?.next;
+    }
+		
+		if(carry > 0)
+		{
+			runningSum.next = new LinkedList(carry);
+		}
+
+    return linkedListSumHead;
+  }
+}
+
