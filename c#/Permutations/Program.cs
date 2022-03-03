04bcb0bc83f5b9fc3ec33c4f6fcd9fe34e96d3b5
vazasantosh@gmail.com <vazasantosh@gmail.com>
2022-03-03 00:06:29 -0600

    Add powerset

---
+++ c#/Permutations/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+  public static List<List<int>> GetPermutations(List<int> array)
+  {
+    var permutations = new List<List<int>>();
+    for (int i = 0; i < array.Count; i++)
+    {
+      int elementPermuteCount = 0;
+      while (elementPermuteCount < array.Count - 1 || (array.Count == 1 && elementPermuteCount < array.Count))
+      {
+        elementPermuteCount++;
+        var currentPermutations = new List<int>();
+        currentPermutations.Add(array[i]);
+        var currentPermutationIndex = i == array.Count - 1 ? i - elementPermuteCount : i + elementPermuteCount;
+
+        while (currentPermutations.Count < array.Count)
+        {
+          if (currentPermutationIndex >= array.Count) currentPermutationIndex = 0;
+          if (currentPermutationIndex != i)
+          {
+            currentPermutations.Add(array[currentPermutationIndex]);
+          }
+          currentPermutationIndex++;
+        }
+        permutations.Add(currentPermutations);
+      }
+
+    }
+    return permutations;
+  }
+}
---
+++ c#/PowerSet/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+  public static List<List<int>> Powerset(List<int> array)
+  {
+
+    var powerSet = new List<List<int>>();
+    powerSet.Add(new List<int>());
+    for (int p = 0; p < array.Count; p++)
+    {
+      FindPowerSet(array, p + 1, powerSet, new HashSet<int>(), 0);
+    }
+
+    return powerSet;
+  }
+
+  public static void FindPowerSet(List<int> array, int power, List<List<int>> powerSet, HashSet<int> currentSet, int nextIdx)
+  {
+    if (currentSet.Count == power)
+    {
+      powerSet.Add(currentSet.ToList());
+      return;
+    }
+
+
+    for (int i = nextIdx; i < array.Count; i++)
+    {
+      currentSet.Add(array[i]);
+      FindPowerSet(array, power, powerSet, currentSet, i + 1);
+      currentSet.Remove(array[i]);
+    }
+  }
+}
