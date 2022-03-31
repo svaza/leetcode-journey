08c2c51d697113a17321036f8307ee857dc7604b
vazasantosh@gmail.com <vazasantosh@gmail.com>
2022-03-31 10:34:19 -0500

    add balanced brackets problem

---
+++ c#/BalancedBrackets/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+  private static HashSet<char> tokenSetOpening = new HashSet<char>() { '(', '[', '{' };
+  private static HashSet<char> tokenSetClosing = new HashSet<char>() { ')', ']', '}' };
+  private static Dictionary<char, char> tokenMap = new Dictionary<char, char>()
+  {
+    { '{', '}' },
+    { '[', ']' },
+    { '(', ')' }
+  };
+
+  public static bool BalancedBrackets(string str)
+  {
+
+    Stack<char> tokens = new Stack<char>(str.Length);
+
+    foreach (var c in str)
+    {
+      if (tokenSetOpening.Contains(c))
+      {
+        tokens.Push(c);
+      }
+      else if (tokenSetClosing.Contains(c) && tokens.Count() > 0)
+      {
+        var tokenSibling = tokens.Pop();
+        if (c != tokenMap[tokenSibling])
+        {
+          return false;
+        }
+      }
+      else if(tokenSetClosing.Contains(c) && tokens.Count() == 0)
+      {
+        return false;
+      }
+    }
+
+    return tokens.Count == 0;
+  }
+
+  public static void Main(string[] args)
+  {
+    Console.WriteLine("Hello World");
+  }
+}
