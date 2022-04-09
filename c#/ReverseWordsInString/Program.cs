6dd4757b577770bfca70f1ddf1f04b5ef6249f1d
vazasantosh@gmail.com <vazasantosh@gmail.com>
2022-04-09 12:28:25 -0500

    reverse words

---
+++ c#/ReverseWordsInString/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+public class Program
+{
+
+  public string ReverseWordsInString(string str)
+  {
+    if (str.Length == 0) return str;
+
+    const char space = (char)32;
+    Stack<string> reverseWordsStack = new Stack<string>(str.Length);
+    char previousChar = str[0];
+    StringBuilder runningStr = new StringBuilder().Append(previousChar);
+    for (int i = 1; i < str.Length; i++)
+    {
+      var currentChar = str[i];
+
+      if ((previousChar == space && currentChar != space) || (previousChar != space && currentChar == space))
+      {
+        reverseWordsStack.Push(runningStr.ToString());
+        runningStr.Clear();
+      }
+
+      runningStr.Append(currentChar);
+      previousChar = currentChar;
+    }
+
+    if (runningStr.Length > 0) reverseWordsStack.Push(runningStr.ToString());
+
+    runningStr.Clear();
+
+    while (reverseWordsStack.Count > 0)
+    {
+      runningStr.Append(reverseWordsStack.Pop());
+    }
+
+
+    return runningStr.ToString();
+  }
+}
+
